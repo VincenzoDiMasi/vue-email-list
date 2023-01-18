abc99ad988a2a10a085353cc00334a65dba89301
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-01-18 15:24:36 +0100

    Finish

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ console.log('JS OK')
 const app = Vue.createApp({
     data(){
         return {
+         emails: [],
+         apiUri : 'https://flynn.boolean.careers/exercises/api/random/mail',
+         emailLot : 10,
 
         }
+    },
+    methods : {
+       getEmailList(){
+          for(let i = 0 ; i < this.emailLot ; i++){
+            axios.get(this.apiUri).then( response =>{
+              this.emails.push(response.data.response);
+            })
+          }
+       }
+    },
+    mounted(){
+        this.getEmailList();
     }
 })
 
